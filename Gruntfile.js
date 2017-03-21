e13d558ed51bc78630f4f097d119ec05812e9be3
Brian Anderson <bqanderson@me.com>
2017-03-21 16:15:29 -0500

    Add new styles and js, create grunt task for deployment.

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Tasks
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: 'public/stylesheets/sass',
+          src: ['**/*.scss'],
+          dest: 'public/stylesheets',
+          ext: '.css'
+        }]
+      }
+    },
+    postcss: {
+      options: {
+        map: false,
+        processor: [
+          require('autoprefixer')({
+            browsers: ['last 2 versions']
+          })
+        ]
+      },
+      dist: {
+        src: 'public/stylesheets/main.css'
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public/stylesheets',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/stylesheets',
+          ext: '.min.css'
+        }]
+      }
+    },
+    uglify: {
+      build: {
+        src: ['public/scripts/dev/*.js'],
+        dest: 'public/scripts/script.min.js',
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'postcss', 'cssmin']
+      },
+      js: {
+        files: '**/*.js',
+        tasks: ['uglify']
+      }
+    }
+  });
+
+  // Load Grunt Plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register Grunt tasks
+  grunt.registerTask('default', ['watch']);
+};
---
+++ public/scripts/dev/testscript.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(function () {
+    $('button').click(function () {
+      alert("Hi, My name is Brian Q...");
+    });
+  });
+})(jQuery);
