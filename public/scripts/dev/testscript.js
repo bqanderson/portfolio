aad06912575aac7267b6b692e3e8f7a6924bb7b3
Brian Anderson <bqanderson@me.com>
2017-03-26 23:43:57 -0500

    Add logo-wrapper to change size of SVG logo

--- public/scripts/dev/testscript.js
+++ public/scripts/dev/testscript.js
@@ -1,7 +1,12 @@
 (function ($) {
   $(function () {
     $('button').click(function () {
-      alert("Hi, My name is Brian Q...");
+      alert("Hi, My name is Brian Q Anderson...");
+    });
+  });
+  $(function() {
+    $('#logo').click(function () {
+      alert("How do you like my new logo?");
     });
   });
 })(jQuery);
