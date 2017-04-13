323d4f27da9703d3504176695f611606fb59c0b6
Brian Anderson <bqanderson@me.com>
2017-04-12 22:58:04 -0500

    Make changes for form on Hello Modal

---
+++ public/scripts/dev/modal-loader.js
@@ -0,0 +1,15 @@
+// Modal loader test
+// 
+// $(document).ready(function(){
+//   var url = '../views/helloModal.html';
+//   $('.modallink').click(function(e){
+//     console.log('Modal Link clicked');
+//     $('.modal-container').load(url, function(result){
+//       $('#helloModal').modal({show: true});
+//     });
+//   });
+// });
+//
+// $(document).on('hidden.bs.modal', '#helloModal', function(){
+//   $('#myModal').remove();
+// });
--- public/scripts/dev/smooth-scroll.js
+++ public/scripts/dev/smooth-scroll.js
@@ -1,7 +1,7 @@
 // Smooth scrolling to anchor tags
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $('a').on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
